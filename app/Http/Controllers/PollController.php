dcaa16e4e06fc0213ba15bea85589dd6dce51a1e
yamauchi5748 <47198760+yamauchi5748@users.noreply.github.com>
2019-06-06 15:00:30 +0900

    [feature] poll_post

---
+++ app/Http/Controllers/PollController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PollController extends Controller
+{
+    public function post(Request $request){
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/poll', "PollController@post");
 
 Auth::routes();
